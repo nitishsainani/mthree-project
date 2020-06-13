f58f053ac937e80fa43c966d2b4a11d36159f4b5
Nitish Sainani <nitish.sainani@gmail.com>
2020-06-13 22:35:23 +0530

    added spring java project

---
+++ backend/stock-exchage-mthree-project/src/main/java/com/mthree/StockExchageMthreeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchageMthreeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockExchageMthreeProjectApplication.class, args);
+	}
+
+}
---
+++ backend/stock-exchage-mthree-project/src/test/java/com/mthree/StockExchageMthreeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchageMthreeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
