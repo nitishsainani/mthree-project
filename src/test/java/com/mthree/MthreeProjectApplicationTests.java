d7400093f97226c065a765899c7c71c74ca0b9c4
Nitish Sainani <nitish.sainani@gmail.com>
2020-06-15 12:43:15 +0530

    created project

---
+++ src/test/java/com/mthree/MthreeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MthreeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
