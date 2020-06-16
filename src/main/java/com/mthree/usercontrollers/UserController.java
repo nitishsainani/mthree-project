11dcff3cca16baa9fb55023700c8c65ed4353477
TaraCanugovi <tcanugovi@gmail.com>
2020-06-16 23:56:01 +0530

    "commited models in controllers"

--- src/main/java/com/mthree/usercontrollers/UserController.java
+++ src/main/java/com/mthree/usercontrollers/UserController.java
@@ -64,12 +64,12 @@ public class UserController {
 		
 	User u=userservice.Login(name,pass);
 	if(u!=null)
-	{   m.addAttribute("loginuser",u);
+	{   m.addAttribute("loginUser",u);
 		return m;
 	}
 	else
 	{
-		m.addAttribute("loginuser","user not found");
+		m.addAttribute("loginUser","user not found");
 		return m;
 	}
 	}
